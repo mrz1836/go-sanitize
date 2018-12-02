43aeacc6460647e9e184d65b9c6cba2f424538f3
mrz1836 <mrz1818@gmail.com>
2018-12-02 18:56:48 -0500

    More work on example method

--- sanitize.go
+++ sanitize.go
@@ -38,10 +38,9 @@ var (
 	urlRegExp                    = regexp.MustCompile(`[^a-zA-Z0-9-_/:.?&=#%]`)                                                   //URL allowed characters
 )
 
-//	Alpha returns only alpha characters (flag for spaces)
-//		input := "My String!"           //Input string
-//		sanitized := Alpha(input,false)	//Run
-//		log.Println(sanitized) 			//MyString
+//	Alpha returns only alpha characters. Set spaces to true if you want to allow space characters. Valid characters are a-z and A-Z.
+//		sanitized := Alpha("My String!",false)
+//		log.Println(sanitized) //MyString
 func Alpha(original string, spaces bool) string {
 
 	//Leave white spaces?
