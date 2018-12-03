466a691fa070a9f306d3cd6a1429ddbca874c54e
mrz1836 <mrz1818@gmail.com>
2018-12-02 19:56:00 -0500

    Added example go file

---
+++ example/example.go
@@ -0,0 +1,20 @@
+/*
+Package example shows how to use the go-sanitize package.
+
+If you have any suggestions or comments, please feel free to open an issue on this project's GitHub page.
+
+Author: MrZ
+*/
+package example
+
+import (
+	"fmt"
+	"github.com/mrz1836/go-sanitize"
+)
+
+func main() {
+	//Run and display
+	fmt.Println("Result:", gosanitize.IPAddress(" 192.168.0.1 "))
+
+	// 192.168.0.1
+}
