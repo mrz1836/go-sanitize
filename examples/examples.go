ff3428aa8d27d3ee70a7f05dfa5e7c8e03a34f51
mrz1836 <mrz1818@gmail.com>
2020-11-29 12:39:01 -0500

    Added basic examples

---
+++ examples/examples.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mrz1836/go-sanitize"
+)
+
+func main() {
+
+	testString := "1-> A simple test string!"
+
+	// Remove spaces, numbers and symbols
+	formattedString := sanitize.Alpha(testString, false)
+	fmt.Printf("removed numbers, spaces & symbols: %s\n", formattedString)
+
+	// Preserve spaces
+	formattedString = sanitize.Alpha(testString, true)
+	fmt.Printf("preserved spaces: %s\n", formattedString)
+
+	// Only numbers
+	formattedString = sanitize.Numeric(testString)
+	fmt.Printf("only numbers: %s\n", formattedString)
+}
