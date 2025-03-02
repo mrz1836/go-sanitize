bae9ab9d4191f7f089ae136e82816de4f9cfb1a5
mrz1836 <mrz1818@gmail.com>
2025-03-02 12:49:52 -0500

    Added a few more examples

--- examples/examples.go
+++ examples/examples.go
@@ -22,5 +22,30 @@ func main() {
 	formattedString = sanitize.Numeric(testString)
 	fmt.Printf("only numbers: %s\n", formattedString)
 
+	// Sanitize a URL
+	urlString := "https://example.com/This/Works?^No&this"
+	sanitizedURL := sanitize.URL(urlString)
+	fmt.Printf("sanitized URL: %s\n", sanitizedURL)
+
+	// Sanitize an XML string
+	xmlString := "<xml>This?</xml>"
+	sanitizedXML := sanitize.XML(xmlString)
+	fmt.Printf("sanitized XML: %s\n", sanitizedXML)
+
+	// Sanitize a URI
+	uriString := "/This/Works?^No&this"
+	sanitizedURI := sanitize.URI(uriString)
+	fmt.Printf("sanitized URI: %s\n", sanitizedURI)
+
+	// Sanitize a time string
+	timeString := "t00:00:00d -EST"
+	sanitizedTime := sanitize.Time(timeString)
+	fmt.Printf("sanitized time: %s\n", sanitizedTime)
+
+	// Sanitize a string for XSS
+	xssString := "<script>alert('test');</script>"
+	sanitizedXSS := sanitize.XSS(xssString)
+	fmt.Printf("sanitized XSS: %s\n", sanitizedXSS)
+
 	// See more in: sanitize_test.go
 }
