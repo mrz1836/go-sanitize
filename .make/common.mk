a5f7d8a10812a1d09801a0c9611c7273f7a972d1
mrz1836 <mrz1818@gmail.com>
2025-06-17 09:18:09 -0400

    fix: lots of updates to common makefile cmds and formatting

--- .make/common.mk
+++ .make/common.mk
@@ -1,33 +1,35 @@
-## Default repository domain name
+# Default repository domain name
 ifndef GIT_DOMAIN
-	override GIT_DOMAIN=github.com
+       override GIT_DOMAIN := github.com
 endif
 
-## Set if defined (alias variable for ease of use)
+# Set if defined (alias variable for ease of use)
 ifdef branch
-	override REPO_BRANCH=$(branch)
-	export REPO_BRANCH
+       override REPO_BRANCH := $(branch)
+       export REPO_BRANCH
 endif
 
-## Do we have git available?
+# Do we have git available?
 HAS_GIT := $(shell command -v git 2> /dev/null)
 
 ifdef HAS_GIT
-	## Do we have a repo?
-	HAS_REPO := $(shell git rev-parse --is-inside-work-tree 2> /dev/null)
-	ifdef HAS_REPO
-		## Automatically detect the repo owner and repo name (for local use with Git)
-		REPO_NAME=$(shell basename "$(shell git rev-parse --show-toplevel 2> /dev/null)")
-		OWNER=$(shell git config --get remote.origin.url | sed 's/git@$(GIT_DOMAIN)://g' | sed 's/\/$(REPO_NAME).git//g')
-		REPO_OWNER=$(shell echo $(OWNER) | tr A-Z a-z)
-		VERSION_SHORT=$(shell git describe --tags --always --abbrev=0)
-		export REPO_NAME, REPO_OWNER, VERSION_SHORT
-	endif
+# Do we have a repo?
+HAS_REPO := $(shell git rev-parse --is-inside-work-tree 2> /dev/null)
+ifdef HAS_REPO
+# Automatically detect the repo owner and repo name (for local use with Git)
+               REPO_NAME := $(shell basename "$(shell git rev-parse --show-toplevel 2> /dev/null)")
+               OWNER := $(shell git config --get remote.origin.url | sed 's/git@$(GIT_DOMAIN)://g' | sed 's/\/$$(REPO_NAME).git//g')
+               REPO_OWNER := $(shell echo $(OWNER) | tr A-Z a-z)
+               VERSION_SHORT := $(shell git describe --tags --always --abbrev=0)
+               export REPO_NAME
+               export REPO_OWNER
+               export VERSION_SHORT
+       endif
 endif
 
-## Set the distribution folder
+# Set the distribution folder
 ifndef DISTRIBUTIONS_DIR
-	override DISTRIBUTIONS_DIR=./dist
+       override DISTRIBUTIONS_DIR := ./dist
 endif
 export DISTRIBUTIONS_DIR
 
@@ -35,13 +37,13 @@ export DISTRIBUTIONS_DIR
 citation: ## Update version in CITATION.cff (citation version=X.Y.Z)
 	@echo "updating CITATION.cff version..."
 	@if [ -z "$(version)" ]; then \
-	    echo "Error: 'version' variable is not set. Please set the 'version' variable before running this target."; \
-	    exit 1; \
+		echo "Error: 'version' variable is not set. Please set the 'version' variable before running this target."; \
+		exit 1; \
 	fi
 	@if [ "$(shell uname)" = "Darwin" ]; then \
-	        sed -i '' -e 's/^version: \".*\"/version: \"$(version)\"/' CITATION.cff; \
+		sed -i '' -e 's/^version: ".*"/version: "$(version)"/' CITATION.cff; \
 	else \
-	        sed -i -e 's/^version: \".*\"/version: \"$(version)\"/' CITATION.cff; \
+		sed -i -e 's/^version: ".*"/version: "$(version)"/' CITATION.cff; \
 	fi
 
 .PHONY: diff
@@ -52,17 +54,26 @@ diff: ## Show the git diff
 
 .PHONY: help
 help: ## Show this help message
-	@egrep -h '^(.+)\:\ ##\ (.+)' ${MAKEFILE_LIST} | column -t -c 2 -s ':#'
+	@grep -Eh '^(.+):\ ##\ (.+)' ${MAKEFILE_LIST} | sort | column -t -c 2 -s ':#'
+
+.PHONY: update-readme
+update-readme: ## Update the README.md with the make commands
+	@echo "updating makefile commands in the README.md..."
+	@TMP=$$(mktemp) && \
+	make --no-print-directory -s help | grep -v '^$$' > $$TMP && \
+	awk -v hf=$$TMP 'BEGIN{skip=0} /<!-- make-help-start -->/{print; print "```text"; while((getline line < hf)>0) print line; print "```"; skip=1; next} /<!-- make-help-end -->/{print; skip=0; next} !skip{print}' README.md > README.md.tmp && \
+	mv README.md.tmp README.md && \
+	rm $$TMP
 
 .PHONY: install-releaser
 install-releaser: ## Install the GoReleaser application
 	@echo "installing GoReleaser..."
-	@curl -sfL https://install.goreleaser.com/github.com/goreleaser/goreleaser.sh | sh
+	@curl -sSfL https://install.goreleaser.com/github.com/goreleaser/goreleaser@latest | sh
 
 .PHONY: release
 release:: ## Full production release (creates release in GitHub)
 	@echo "releasing..."
-	@test $(github_token)
+	@test -n "$(github_token)"
 	@export GITHUB_TOKEN=$(github_token) && goreleaser --rm-dist
 
 .PHONY: release-test
@@ -74,14 +85,6 @@ release-test: ## Full production test release (everything except deploy)
 release-snap: ## Test the full release (build binaries)
 	@echo "creating a release snapshot..."
 	@goreleaser --snapshot --skip-publish --rm-dist
-
-.PHONY: replace-version
-replace-version: ## Replaces the version in HTML/JS (pre-deploy)
-	@echo "replacing version..."
-	@test $(version)
-	@test "$(path)"
-	@find $(path) -name "*.html" -type f -exec sed -i '' -e "s/{{version}}/$(version)/g" {} \;
-	@find $(path) -name "*.js" -type f -exec sed -i '' -e "s/{{version}}/$(version)/g" {} \;
 
 .PHONY: tag
 tag: ## Generate a new tag and push (tag version=0.0.0)
