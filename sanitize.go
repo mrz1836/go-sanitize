54c0fe6b0544ff4462cd13b7cf2025cfa5c53230
Mr. Z <mrz1836@users.noreply.github.com>
2025-06-17 14:34:38 -0400

    refactor(sanitize): optimize URL sanitizer

--- sanitize.go
+++ sanitize.go
@@ -40,7 +40,6 @@ var (
 	scriptRegExp          = regexp.MustCompile(`(?i)<(script|iframe|embed|object)[^>]*>.*</(script|iframe|embed|object)>`) // Scripts and embeds
 	singleLineRegExp      = regexp.MustCompile(`(\r)|(\n)|(\t)|(\v)|(\f)`)                                                 // Carriage returns, line feeds, tabs, for single line transition
 	uriRegExp             = regexp.MustCompile(`[^a-zA-Z0-9-_/?&=#%]`)                                                     // URI allowed characters
-	urlRegExp             = regexp.MustCompile(`[^a-zA-Z0-9-_/:.,?&@=#%]`)                                                 // URL allowed characters
 	wwwRegExp             = regexp.MustCompile(`(?i)www.`)                                                                 // For removing www
 )
 
@@ -665,7 +664,17 @@ func URI(original string) string {
 //
 // See more usage examples in the `sanitize_test.go` file.
 func URL(original string) string {
-	return string(urlRegExp.ReplaceAll([]byte(original), emptySpace))
+	var b strings.Builder
+	b.Grow(len(original))
+	for _, r := range original {
+		if unicode.IsLetter(r) || unicode.IsDigit(r) ||
+			r == '-' || r == '_' || r == '/' || r == ':' ||
+			r == '.' || r == ',' || r == '?' || r == '&' ||
+			r == '@' || r == '=' || r == '#' || r == '%' {
+			b.WriteRune(r)
+		}
+	}
+	return b.String()
 }
 
 // XML returns a string without any XML tags.
